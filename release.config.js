17fe704ad6be28f6b011cbb0c4a8c0b7de1fde52
Paul Ochieng Levi <ochieng442@outlook.com>
2025-05-02 19:15:38 +0300

    fix: update package dependencies and publishing configuration

--- release.config.js
+++ release.config.js
@@ -10,31 +10,16 @@ module.exports = {
       },
     ],
     [
-      '@semantic-release/github',
-      {
-        assets: [{ path: 'packages/*/dist/*.tgz', label: 'NPM distribution' }],
-        successComment: true,
-        failTitle: true,
-      },
-    ],
-    // Core package publish (must be first since others depend on it)
-    [
       '@semantic-release/npm',
       {
         pkgRoot: './packages/core',
-        tarballDir: 'dist',
         npmPublish: true,
-        // Ensure core package is published before others
-        verifyConditions: ['@semantic-release/npm', { npmPublish: true }],
-        prepare: ['@semantic-release/npm', { npmPublish: true }],
       },
     ],
-    // Other packages that depend on core
     [
       '@semantic-release/npm',
       {
         pkgRoot: './packages/react',
-        tarballDir: 'dist',
         npmPublish: true,
       },
     ],
@@ -42,7 +27,6 @@ module.exports = {
       '@semantic-release/npm',
       {
         pkgRoot: './packages/react-native',
-        tarballDir: 'dist',
         npmPublish: true,
       },
     ],
@@ -50,7 +34,6 @@ module.exports = {
       '@semantic-release/npm',
       {
         pkgRoot: './packages/vue',
-        tarballDir: 'dist',
         npmPublish: true,
       },
     ],
@@ -64,4 +47,4 @@ module.exports = {
     ],
   ],
   preset: 'angular',
-};
+}
