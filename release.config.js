f7b0544cee53df748e81266e5d29a5fc308302cb
Paul Ochieng Levi <ochieng442@outlook.com>
2025-05-04 02:39:39 +0300

    fix: update NPM configuration for authentication and improve release process

--- release.config.js
+++ release.config.js
@@ -1,43 +1,75 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    ['@semantic-release/commit-analyzer', {
-      preset: 'angular',
-      releaseRules: [
-        { type: 'docs', scope: 'README', release: 'patch' },
-        { type: 'refactor', release: 'patch' },
-        { type: 'style', release: 'patch' },
-        { type: 'fix', release: 'patch' },
-        { type: 'feat', release: 'minor' },
-        { type: 'perf', release: 'patch' }
-      ],
-      parserOpts: {
-        noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
-      }
-    }],
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'docs', scope: 'README', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'fix', release: 'patch' },
+          { type: 'feat', release: 'minor' },
+          { type: 'perf', release: 'patch' },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', {
-      changelogFile: 'CHANGELOG.md',
-    }],
-    ['@semantic-release/npm', {
-      pkgRoot: './packages/core',
-    }],
-    ['@semantic-release/npm', {
-      pkgRoot: './packages/react',
-    }],
-    ['@semantic-release/npm', {
-      pkgRoot: './packages/react-native',
-    }],
-    ['@semantic-release/npm', {
-      pkgRoot: './packages/vue',
-    }],
-    ['@semantic-release/git', {
-      assets: [
-        'CHANGELOG.md',
-        'package.json',
-        'packages/*/package.json'
-      ],
-      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-    }],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        pkgRoot: './packages/core',
+        npmPublish: true,
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        pkgRoot: './packages/react',
+        npmPublish: true,
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        pkgRoot: './packages/react-native',
+        npmPublish: true,
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        pkgRoot: './packages/vue',
+        npmPublish: true,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'packages/*/package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+  verifyConditions: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
   ],
 };
