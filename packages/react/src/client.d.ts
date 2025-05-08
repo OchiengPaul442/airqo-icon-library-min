094b9e4088201b5766400d9fa69afef042873db3
Paul Ochieng Levi <ochieng442@outlook.com>
2025-05-08 19:12:37 +0300

    Refactor code structure for improved readability and maintainability

--- packages/react/src/client.d.ts
+++ packages/react/src/client.d.ts
@@ -1,16 +1,32 @@
 import React from 'react';
-import { IconProps } from './withIconProps';
-import enhancedIcons from './enhanceIcons';
-import withIconProps from './withIconProps';
+import type { IconProps } from './withIconProps';
 
+/**
+ * Wraps an icon component with 'use client' directive for Next.js support
+ * Use this wrapper when you need interactive icons with event handlers in Next.js
+ *
+ * @example
+ * import { AlertCircle } from '@airqo-icons-min/react';
+ * import { ClientIcon } from '@airqo-icons-min/react/client';
+ *
+ * // In your component:
+ * <ClientIcon icon={AlertCircle} onClick={() => alert('clicked')} />
+ */
 export declare const ClientIcon: React.FC<
   {
     icon: React.ComponentType<IconProps>;
   } & IconProps
 >;
 
+// Re-export everything from index
 export * from './index';
-export { enhancedIcons as icons };
-export { withIconProps };
-export type { IconProps };
-export default enhancedIcons;
+
+// Export types
+export { withIconProps } from './withIconProps';
+export type { IconProps } from './withIconProps';
+
+// Export enhanced icons
+export { default as icons } from './enhanceIcons';
+
+// Default export
+export { default } from './enhanceIcons';
